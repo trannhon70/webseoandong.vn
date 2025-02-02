9d8d72dee32bddf83ad760f832f259a78ff0d0c3
trannhon70 <kevintran351996@gmail.com>
2025-02-02 11:58:32 +0700

    update

--- inc/footer.php
+++ inc/footer.php
@@ -1,6 +1,31 @@
 
-<footer>
-footer
+<footer class="footer">
+    <div class="footer__container" >
+        <div class="footer__container-title">đặt lịch tư vấn ngay</div>
+        <div class="footer__container-text">Để giải đáp những thắc mắc về sức khỏe</div>
+        <div class="footer__container-body">
+            <div class="footer__container-body-left">
+                <img loading="lazy" width="100%" height="auto" src="<?php echo $local ?>/images/users/chuyenmuc.webp" alt="...">
+            </div>
+            <div class="footer__container-body-right">
+                <img loading="lazy" width="100%" height="auto" src="<?php echo $local ?>/images/users/bacsi.webp" alt="...">
+            </div>
+        </div>
+        <div class="footer__container-bottom" >
+            <a href="<?php echo $local ?>">
+                <img loading="lazy" width="250px" height="auto" src="<?php echo $local ?>/images/icons/icon_dangky.webp" alt="...">
+            </a>
+            <a href="<?php echo $local ?>">
+                <img loading="lazy" width="250px" height="auto" src="<?php echo $local ?>/images/icons/icon_tuvan.webp" alt="...">
+            </a>
+            <a href="<?php echo $local ?>">
+                <img loading="lazy" width="250px" height="auto" src="<?php echo $local ?>/images/icons/icon_chatzalo.webp" alt="...">
+            </a>
+        </div>
+    </div>
+    <div class="footer__bg" >
+
+    </div>
 </footer>
 
 <script defer src="<?php echo $local ?>/js/carousel.min.js" ></script>
